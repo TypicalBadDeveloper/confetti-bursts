8acd5da38819c34cf41acfb82779364586ee43ee
TypicalBadDeveloper <nathanmarchant@protonmail.com>
2022-12-17 15:51:23 -0600

    2 more

---
+++ bigsingle.js
@@ -0,0 +1,140 @@
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/tsparticles/2.6.0/tsparticles.bundle.min.js";
+script.onload = function(){
+  tsParticles.load("tsparticles", {
+  "fullScreen": {
+    "zIndex": 1
+  },
+  "particles": {
+    "number": {
+      "value": 0
+    },
+    "color": {
+      "value": [
+        "#00FFFC",
+        "#FC00FF",
+        "#fffc00"
+      ]
+    },
+    "shape": {
+      "type": [
+        "circle",
+        "square"
+      ],
+      "options": {}
+    },
+    "opacity": {
+      "value": 1,
+      "animation": {
+        "enable": true,
+        "minimumValue": 0,
+        "speed": 2,
+        "startValue": "max",
+        "destroy": "min"
+      }
+    },
+    "size": {
+      "value": 4,
+      "random": {
+        "enable": true,
+        "minimumValue": 2
+      }
+    },
+    "links": {
+      "enable": false
+    },
+    "life": {
+      "duration": {
+        "sync": true,
+        "value": 5
+      },
+      "count": 1
+    },
+    "move": {
+      "enable": true,
+      "gravity": {
+        "enable": true,
+        "acceleration": 10
+      },
+      "speed": {
+        "min": 10,
+        "max": 20
+      },
+      "decay": 0.1,
+      "direction": "none",
+      "straight": false,
+      "outModes": {
+        "default": "destroy",
+        "top": "none"
+      }
+    },
+    "rotate": {
+      "value": {
+        "min": 0,
+        "max": 360
+      },
+      "direction": "random",
+      "move": true,
+      "animation": {
+        "enable": true,
+        "speed": 60
+      }
+    },
+    "tilt": {
+      "direction": "random",
+      "enable": true,
+      "move": true,
+      "value": {
+        "min": 0,
+        "max": 360
+      },
+      "animation": {
+        "enable": true,
+        "speed": 60
+      }
+    },
+    "roll": {
+      "darken": {
+        "enable": true,
+        "value": 25
+      },
+      "enable": true,
+      "speed": {
+        "min": 15,
+        "max": 25
+      }
+    },
+    "wobble": {
+      "distance": 15,
+      "enable": true,
+      "move": true,
+      "speed": {
+        "min": -15,
+        "max": 15
+      }
+    }
+  },
+  "emitters": {
+    "life": {
+      "count": 1,
+      "duration": 0.1,
+      "delay": 0.4
+    },
+    "position": {
+      "x": 50,
+      "y": 50
+    },
+    "rate": {
+      "delay": 0.1,
+      "quantity": 1000
+    },
+    "size": {
+      "width": 0,
+      "height": 0
+    }
+  }
+});
+};
+document.body.appendChild(script);
---
+++ spray.js
@@ -0,0 +1,137 @@
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/tsparticles/2.6.0/tsparticles.bundle.min.js";
+script.onload = function(){
+  tsParticles.load("tsparticles", {
+"fullScreen": {
+  "zIndex": 1
+},
+"emitters": {
+  "position": {
+    "x": 50,
+    "y": 100
+  },
+  "life": {
+    "count": 40,
+    "duration": 0.1,
+    "delay": 0
+  },
+  "rate": {
+    "quantity": 5,
+    "delay": 0.15
+  }
+},
+"particles": {
+  "color": {
+    "value": [
+      "#1E00FF",
+      "#FF0061",
+      "#E1FF00",
+      "#00FF9E"
+    ]
+  },
+  "move": {
+    "decay": 0.05,
+    "direction": "top",
+    "enable": true,
+    "gravity": {
+      "enable": true
+    },
+    "outModes": {
+      "top": "none",
+      "default": "destroy"
+    },
+    "speed": {
+      "min": 50,
+      "max": 100
+    }
+  },
+  "number": {
+    "value": 0
+  },
+  "opacity": {
+    "value": 1
+  },
+  "rotate": {
+    "value": {
+      "min": 0,
+      "max": 360
+    },
+    "direction": "random",
+    "animation": {
+      "enable": true,
+      "speed": 30
+    }
+  },
+  "tilt": {
+    "direction": "random",
+    "enable": true,
+    "value": {
+      "min": 0,
+      "max": 360
+    },
+    "animation": {
+      "enable": true,
+      "speed": 30
+    }
+  },
+  "size": {
+    "value": 3,
+    "animation": {
+      "enable": true,
+      "startValue": "min",
+      "count": 1,
+      "speed": 16,
+      "sync": true
+    }
+  },
+  "roll": {
+    "darken": {
+      "enable": true,
+      "value": 25
+    },
+    "enlighten": {
+      "enable": true,
+      "value": 25
+    },
+    "enable": true,
+    "speed": {
+      "min": 5,
+      "max": 15
+    }
+  },
+  "wobble": {
+    "distance": 30,
+    "enable": true,
+    "speed": {
+      "min": -7,
+      "max": 7
+    }
+  },
+  "shape": {
+    "type": [
+      "circle",
+      "square"
+    ],
+    "options": {}
+  }
+},
+"responsive": [
+  {
+    "maxWidth": 1024,
+    "options": {
+      "particles": {
+        "move": {
+          "speed": {
+            "min": 33,
+            "max": 66
+          }
+        }
+      }
+    }
+  }
+]
+});
+};
+document.body.appendChild(script);
